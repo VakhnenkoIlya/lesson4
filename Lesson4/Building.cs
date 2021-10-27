11a170b3234c82fbb2161dcc0964133d7e2932f1
VakhnenkoIlya <mazerfaker@list.ru>
2021-10-27 21:39:57 +0300

    1

---
+++ Lesson4/Building.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Building
+    {
+        private readonly int number;
+        private int height;
+        private int floorCount;
+        private int apartamentCount;
+        private int entranceCount;
+        private int nextNumber = 1;
+        void CreateNumber()
+        {
+            nextNumber++;
+        }
+
+        public Building()
+        {
+            number = nextNumber;
+            CreateNumber();
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+        public int GetHeight()
+        {
+            return height;
+        }
+
+        public void SetHeight(int height)
+        {
+            this.height = height;
+        }
+        public int GetFloorCount()
+        {
+            return floorCount;
+        }
+
+        public void SetFloorCount(int floorCount)
+        {
+            this.floorCount = floorCount;
+        }
+        public int GetApartamentCount()
+        {
+            return apartamentCount;
+        }
+
+        public void SetApartamentCount(int apartamentCount)
+        {
+            this.apartamentCount = apartamentCount;
+        }
+        public int GetEntanceCount()
+        {
+            return entranceCount;
+        }
+
+        public void SetEntranceCount(int entranceCount)
+        {
+            this.entranceCount = entranceCount;
+        }
+        public double CalculationOfFloorHeight(int height, int floorCount)
+        {
+            double a = height;
+            double b = floorCount;
+           return a/b;
+        }
+        public int CalculationApartamentInEntrance(int apartamentCount, int entranceCount)
+        {
+            return apartamentCount / entranceCount;
+        }
+        public int CalculationApartamentInFloor(int floorCount, int apartamentInEntrance)
+        {
+            return apartamentInEntrance/ floorCount; 
+        }
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building1 = new();
+            building1.SetHeight(10);
+            building1.SetFloorCount(4);
+            building1.SetApartamentCount(16);
+            building1.SetEntranceCount(2);
+            Console.WriteLine($"Номер дома {building1.GetNumber()}\n" +
+                $"Высота дома { building1.GetHeight()} m\n" +
+                $"Количество этажей {building1.GetFloorCount()}\n" +
+                $"Количество квартир {building1.GetApartamentCount()}\n" +
+                $"Количество подъездов {building1.GetEntanceCount()}\n" +
+                $"Высота одного этажа {building1.CalculationOfFloorHeight(10, 4)} m\n" +
+                $"Квартир в подъезде {building1.CalculationApartamentInEntrance(16, 2)}\n" +
+                $"Квартир на этаже {building1.CalculationApartamentInFloor(4, building1.CalculationApartamentInEntrance(16, 2))}");
+           
+        }
+    }
+
